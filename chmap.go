e18fe7028b8374bddd3cdb2891c3d497286b1448
SemihBKGR <buyukgungorsemih@gmail.com>
2022-01-27 21:44:11 +0300

    changes underlying data structure to linked list

--- chmap.go
+++ chmap.go
@@ -1,103 +1,89 @@
 package chmap
 
 import (
-	"github.com/mitchellh/hashstructure/v2"
+	"fmt"
 	"hash/fnv"
 	"sync"
 )
 
-const defaultBucketCount = 16
+const loadFactor = 0.75
+const defaultCapacity = 16
 
-type Hasher interface {
-	Hash() uint64
+type node struct {
+	hash  uint32
+	key   string
+	value interface{}
+	next  *node
+}
+
+type bucket struct {
+	sync.RWMutex
+	node *node
 }
 
 type ConcurrentHashMap struct {
-	bucketCount int
-	buckets     []*Bucket
-}
-
-type Bucket struct {
-	sync.RWMutex
-	internal map[interface{}]interface{}
+	capacity   uint32
+	loadFactor float32
+	buckets    []*bucket
 }
 
 func New() (chm ConcurrentHashMap) {
-	chm.bucketCount = defaultBucketCount
-	chm.buckets = make([]*Bucket, defaultBucketCount)
-	for i := 0; i < defaultBucketCount; i++ {
-		chm.buckets[i] = &Bucket{internal: make(map[interface{}]interface{})}
+	chm.capacity = defaultCapacity
+	chm.loadFactor = loadFactor
+	chm.buckets = make([]*bucket, chm.capacity)
+	for i := 0; i < int(chm.capacity); i++ {
+		chm.buckets[i] = &bucket{}
 	}
 	return
 }
 
-func (m *ConcurrentHashMap) Put(key interface{}, value interface{}) {
-	bucket := m.getBucket(key)
-	bucket.Lock()
-	defer bucket.Unlock()
-	bucket.internal[key] = value
-}
-
-func (m *ConcurrentHashMap) Get(key interface{}) (val interface{}, ok bool) {
-	bucket := m.getBucket(key)
-	bucket.RLock()
-	defer bucket.RUnlock()
-	val, ok = bucket.internal[key]
-	return
-}
-
-func (m *ConcurrentHashMap) Contains(key interface{}) (ok bool) {
-	bucket := m.getBucket(key)
-	bucket.RLock()
-	defer bucket.RUnlock()
-	_, ok = bucket.internal[key]
-	return
-}
-
-func (m *ConcurrentHashMap) Count() int {
-	count := 0
-	for i := 0; i < m.bucketCount; i++ {
-		bucket := m.getBucket(i)
-		bucket.RLock()
-		count += len(bucket.internal)
-		bucket.RUnlock()
-	}
-	return count
-}
-
-func (m *ConcurrentHashMap) Remove(key interface{}) {
-	bucket := m.getBucket(key)
-	bucket.Lock()
-	defer bucket.Unlock()
-	delete(bucket.internal, key)
-}
-
-func (m *ConcurrentHashMap) Clear() {
-	for i := 0; i < m.bucketCount; i++ {
-		bucket := m.getBucket(i)
-		bucket.Lock()
-		for k := range bucket.internal {
-			delete(bucket.internal, k)
+func (m *ConcurrentHashMap) Put(key string, value interface{}) {
+	h := hash(key)
+	fmt.Println(h)
+	b := m.buckets[h%m.capacity]
+	b.Lock()
+	defer b.Unlock()
+	if b.node == nil {
+		b.node = &node{
+			hash:  h,
+			key:   key,
+			value: value,
+			next:  nil,
 		}
-		bucket.Unlock()
+	} else {
+		n := b.node
+		for n.next != nil {
+			n = n.next
+		}
+		n.next = &node{
+			hash:  h,
+			key:   key,
+			value: value,
+			next:  nil,
+		}
 	}
 }
 
-func (m *ConcurrentHashMap) getBucket(key interface{}) *Bucket {
-	hash := getHash(key)
-	return m.buckets[int(hash)&m.bucketCount]
+func (m *ConcurrentHashMap) Get(key string) (val interface{}, ok bool) {
+	h := hash(key)
+	fmt.Println(h)
+	b := m.buckets[h%m.capacity]
+	b.RLock()
+	defer b.RUnlock()
+	n := b.node
+	for n != nil {
+		if n.key == key {
+			fmt.Println("found")
+			return n.value, true
+		}
+		n = n.next
+	}
+	fmt.Println("not found")
+	return nil, false
 }
 
-func getHash(key interface{}) (hash uint64) {
-	switch k := key.(type) {
-	case Hasher:
-		hash = k.Hash()
-	case string:
-		h := fnv.New32a()
-		_, _ = h.Write([]byte(k))
-		hash = uint64(h.Sum32())
-	default:
-		hash, _ = hashstructure.Hash(key, hashstructure.FormatV2, nil)
-	}
-	return
+func hash(key string) uint32 {
+	h := fnv.New32a()
+	_, _ = h.Write([]byte(key))
+	return h.Sum32()
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/SemihBKGR/chmap
 
 go 1.17
-
-require github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
